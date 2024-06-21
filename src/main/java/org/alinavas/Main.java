9961318805262d8ce037f1594d3809d5bae340fd
Alina Vasilenko <alinavas.qa@gmail.com>
2024-06-21 18:43:35 +0300

    21.06.24 Создание 4-х методов и вывод их на экран

--- src/main/java/org/alinavas/Main.java
+++ src/main/java/org/alinavas/Main.java
@@ -1,19 +1,47 @@
 package org.alinavas;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void printThreeWords() {
+        System.out.println("Orange\nBanana\nApple");
+    }
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 9;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+                System.out.println("Сумма отрицатльная");
+            }
         }
+    public static void printColor() {
+        int value = 101;
+        if (value <= 0){
+            System.out.println("Красный");
+
+        } if (value > 0 && value <= 100){
+            System.out.println("Желтый");
+        }
+        if (value > 100){
+            System.out.println("Зеленый");
+        }
+
+    }
+    public static void compareNumbers() {
+        int a = 100;
+        int b = 100;
+        if (a > b || a == b){
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a < b");
+        }
+
     }
 }
-
-//*comment*//--
