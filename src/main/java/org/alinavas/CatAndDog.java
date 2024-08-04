2af6b611b7fe6902c415dd0449498156d6d3266e
Alina Vasilenko <alinavas.qa@gmail.com>
2024-08-04 16:55:02 +0300

    4.08 Полная каша.

--- src/main/java/org/alinavas/CatAndDog.java
+++ src/main/java/org/alinavas/CatAndDog.java
@@ -6,7 +6,7 @@ dogBobik.run(150); → 'Бобик пробежал 150 м.');
 У каждого животного есть ограничения на действия (бег: кот 200 м., собака 500 м.; плавание: кот
 не умеет плавать, собака 10 м.).
 Добавить подсчет созданных котов, собак и животных.
-Расширить задачу, добавив для котов возможность кушать из миски, выполнив следующие пункты:
+Расширить задачу, добавив для котов возможность есть из миски, выполнив следующие пункты:
 ● Сделать так, чтобы в миске с едой не могло получиться отрицательного количества еды
 (например, в миске 10 еды, а кот пытается поесть 15-20).
 ● Каждому коту нужно добавить поле сытость (когда создаем котов, они голодны). Если коту
@@ -30,18 +30,75 @@ public class CatAndDog {
         sam.swim(300);
         sam.swim(10);
         sharik.run(900);
-        Cat murka = new Cat("Мурка");
-        Cat belka = new Cat("Белка");
+        Cat murka = new Cat("Мурка", false);
+        Cat belka = new Cat("Белка", false);
         murka.run(200);
         murka.swim(10);
         belka.run(300);
+        Cat[] cats = new Cat[5];
+        cats[0] = new Cat("Vasilij", false);
+        cats[1] = new Cat("Snezhok", false);
+        cats[2] = new Cat("Kuzma", false);
+        cats[3] = new Cat("Bombilo", false);
+        cats[4] = new Cat("Kote", false);
+        Cat.Bowl bowl1 = new Cat.Bowl(100);
+        bowl1.info();
+       // Cat.Bowl.eat(40);
+        //plate=bowl1
+        /*for (int i = 0; i < cats.length; i++) {
+            if (Cat.Bowl.cats[i].eat < bowl1.food){
+                cats[i].eat(plate);
+                cats[i].fullness = true;
+                System.out.println("Котик " + cats[i].name + " покушал!");
+            } else {
+                System.out.println("Котик " + cats[i].name + " не поел!");
+            }
+        }*/
+        bowl1.eat(2);
+        bowl1.eat(6);
+        bowl1.info();
+        bowl1.eat(90);
+        bowl1.info();
+
+        /*Cat murka = new Cat("Мурка", false);
+        Cat belka = new Cat("Белка", false);
+        murka.run(200);
+        murka.swim(10);
+        belka.run(300);*/
+
+
+
         System.out.println("Количество животных: " + Animal.getCountAnimal());
         System.out.println("Количество кошек: " + Cat.getCountCat());
-        System.out.println("Количество кошек: " + Dog.getCountDog());
-        Cat.Bowl bowl1 = new Cat.Bowl(50);
-        bowl1.downFood(40);
-        Cat.Bowl bowl2 = new Cat.Bowl(50);
-        bowl2.downFood(51);
+        System.out.println("Количество cобак: " + Dog.getCountDog());
+        //Cat.Bowl bowl1 = new Cat.Bowl(50);
+        //bowl1.downFood(40);
+       // Cat.Bowl bowl1 = new Cat.Bowl(100);
+        //bowl1.downFood(51);
+        //bowl2.addFood(30);
+        //Cat.Bowl bowl1 = new Cat.Bowl(100);
+
+        /*Cat[] cats = new Cat[5];
+        cats[0] = new Cat("Vasilij", false);
+        cats[1] = new Cat("Snezhok", false);
+        cats[2] = new Cat("Kuzma", false);
+        cats[3] = new Cat("Bombilo", false);
+        cats[4] = new Cat("Kote", false);
+        Cat.Bowl bowl1 = new Cat.Bowl(100);
+        Cat.Bowl.eat(5);
+        Cat.Bowl.eat(90);*/
+
+        /*for (int i = 0; i < cats.length; i++) {
+            if (cats[i].satiety == false && cats[i].eat < bowl1.food){
+                //cats[i].eat(plate);
+                cats[i].satiety = true;
+                System.out.println("Котик " + cats[i].name + " поел");
+            } else {
+                System.out.println("Котик " + cats[i].name + " не поел");
+            }
+        }*/
+        //Cat.bowl.addFood(20);
+
     }
 }
 class Animal {
@@ -85,8 +142,12 @@ class Dog extends Animal {
 class Cat extends Animal {
     String kind = "Кошка";
     static int countCat = 0;
-    Cat (String name) {
+    public static boolean satiety;
+    //public static int eat;
+    //int food;
+    Cat (String name, boolean satiety) {
         super(name);
+       this.satiety = satiety;
         countCat++;
     }
     public void run(int lengthRun) {
@@ -98,18 +159,33 @@ class Cat extends Animal {
     public static int getCountCat() {
         return countCat;
     }
-    static class Bowl{ // При объявл внутр класса присут. служ static(вложенный класс)
-        int food;
-        static boolean satiety = false;
+   /* public void downFood (){
+        int difference = food - eat;
+        System.out.println(difference < 0 ? "В миске нет столько еды, нужно лопать меньше, " + "кот сыт: " + satiety : "В миске еще осталось еды: " + difference + ", кот сыт: " + !satiety);
+    }*/
+
+    public static class Bowl{ // При объявл внутр класса присут. служ static(вложенный класс)
+        public int food;
         Bowl(int food) {
             this.food = food;
         }
-        void downFood(int x) {
-            int difference = food - x;
-            if (difference < 0)
-                System.out.println("В миске нет столько еды, нужно лопать меньше, " + "кот сыт: " + satiety);
-            else
-                System.out.println("В миске еще осталось еды: " + difference + ", кот сыт: " + !satiety);
+      /* public void eat (int downFood) {
+            int difference = food - downFood;
+            System.out.println(difference < 0 ? "В миске нет столько еды, нужно лопать меньше, " + "кот сыт: " + satiety : "В миске еще осталось еды: " + difference + ", кот сыт: " + !satiety);
+        }
+        public void addFood(int add) {
+            int difference = food + add;
+            System.out.println("Еды в миске стало: " + difference);
+        }*/
+      public void eat(int n) {
+          food -= n;
+          System.out.println(food);
+      }
+        public void addFood(int x) {
+            food += x;
+        }
+        public void info() {
+            System.out.println("Всего еды: " + food);
         }
     }
 }
